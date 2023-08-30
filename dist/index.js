6c906ece13ac547c4cc088bb9bfaabc596cc0d60
ThiagoGlady <thiagoglady@gmail.com>
2023-08-30 12:08:32 -0300

    feat: :rocket: initializing

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+const calcular_somar = (a, b) => a + b;
+console.log(calcular_somar(3, 7));
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const calcular_somar = (a: number, b: number): number => a + b;
+
+console.log(calcular_somar(3, 7));
